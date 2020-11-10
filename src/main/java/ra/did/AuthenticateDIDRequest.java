5f854de8207bfc76ff7d879ac9ecd63f0aff5f5a
objectorange <brian@resolvingarchitecture.io>
2020-11-10 10:51:05 -0500

    added request class from common

---
+++ src/main/java/ra/did/AuthenticateDIDRequest.java
@@ -0,0 +1,16 @@
+package ra.did;
+
+import ra.common.identity.DID;
+import ra.common.service.ServiceMessage;
+
+public class AuthenticateDIDRequest extends ServiceMessage {
+    public static final int DID_REQUIRED = 1;
+    public static final int DID_FINGERPRINT_REQUIRED = 2;
+    public static final int DID_PASSPHRASE_REQUIRED = 3;
+    public static final int DID_PASSPHRASE_HASH_ALGORITHM_UNKNOWN = 4;
+    public static final int DID_USERNAME_UNKNOWN = 5;
+
+    public DID did;
+    public Boolean autogenerate = true;
+
+}
